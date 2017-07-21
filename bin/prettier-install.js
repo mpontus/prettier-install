7cce08c44c435250c61b9f141a2c7c7bec294535
Michael Pontus <m.pontus@gmail.com>
2017-07-21 15:45:08 +0300

    Install, fix, and commit working.

--- bin/prettier-install.js
+++ bin/prettier-install.js
@@ -1,16 +1,96 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util')
 const which = promisify(require('which'));
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+
+function exec(command) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, {
+      shell: true,
+      stdio: 'inherit',
+    });
+
+    child.on('exit', (code, signal) => {
+      if (code === 0) {
+        resolve();
+
+        return;
+      }
+
+      const error = new Error();
+
+      Object.assign(error, { code, signal });
+      reject(error);
+    })
+  })
+}
 
 class PrettierInstall {
   async run() {
-    const useYarn = await this.detectYarn();
-    await this.installPrettierIfNeeded(useYarn);
-    // await this.runPrettier();
-    // await this.commitChanges();
+    const useGit = await this.detectGit();
+
+    if (useGit) {
+      await this.ensureNoUncommitedChanges();
+    }
+
+    let useYarn;
+    const prettierInstalled = this.isPrettierInstalled();
+
+    if (!prettierInstalled) {
+      useYarn = this.detectYarn();
+      await this.installPrettier(useYarn);
+    }
+
+    await this.runPrettier();
+
+    if (useGit) {
+      await this.commitChanges(prettierInstalled, useYarn);
+    }
+  }
+
+  async detectGit() {
+      try {
+        which('git')
+      } catch (error) {
+        return false;
+      }
+
+      const gitDirectoryPath = path.resolve(
+        process.cwd(),
+        '.git',
+      )
+      const gitDirectoryExists = await promisify(fs.exists)(gitDirectoryPath);
+
+      if (!gitDirectoryExists) {
+        return false;
+      }
+
+      return true;
+  }
+
+  async ensureNoUncommitedChanges() {
+    try {
+      await exec('git diff-index --quiet HEAD --')
+    } catch (error) {
+      console.error('Commit changes in your current tree before proceeding')
+
+      process.exit(1);
+    }
+  }
+
+  isPrettierInstalled() {
+    try {
+      const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+      const metadata = require(packageJsonPath);
+      const { dependencies, devDependencies } = metadata;
+
+      return 'prettier' in Object.assign({}, dependencies, devDependencies);
+    } catch (error) {
+      return false;
+    }
   }
 
   async detectYarn() {
@@ -42,30 +122,43 @@ class PrettierInstall {
     return useYarn;
   }
 
-  async installPrettierIfNeeded(useYarn) {
-    if (this.isPrettierInstalled()) {
-      console.log('Prettier is already installed.')
+  async installPrettier(useYarn) {
+      console.log('Installing prettier');
 
-      return;
-    }
-
-    console.log('Installing prettier');
-
-    await promisify(exec)(
-      useYarn
-        ? 'yarn add --dev prettier'
-        : 'npm install --save-dev prettier'
-    )
-
-    console.log('Prettier installed');
+      await exec(
+        useYarn
+          ? 'yarn add --dev prettier'
+          : 'npm install --save-dev prettier'
+      )
   }
 
-  isPrettierInstalled() {
-    const packageJsonPath = path.resolve(process.cwd(), 'package.json');
-    const metadata = require(packageJsonPath);
-    const { dependencies, devDependencies } = metadata;
+  async runPrettier() {
+    console.log('Running prettier');
 
-    return 'prettier' in Object.assign({}, dependencies, devDependencies);
+    const prettierCmd = path.resolve(
+      process.cwd(),
+      'node_modules/.bin/prettier'
+    );
+
+    await exec(
+      `${prettierCmd} --write **/*.js`
+    )
+  }
+
+  async commitChanges(wasPrettierInstalled, useYarn) {
+    if (!wasPrettierInstalled) {
+      await exec('git add package.json');
+
+      if (useYarn) {
+        await exec('git add yarn.lock')
+      } else {
+        await exec('git add package-lock.json');
+      }
+    }
+
+    await exec(
+      'git commit --all --edit --message "Installed prettier"'
+    )
   }
 }
 
