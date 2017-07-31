9b0dcb78e9d18687a46fe596492729b1d61a57f0
Michael Pontus <m.pontus@gmail.com>
2017-07-31 17:47:50 +0300

    Feedback now supports prompt

--- src/feedback.js
+++ src/feedback.js
@@ -1,6 +1,46 @@
+const tty = require('tty');
+
 class Feedback {
+  constructor(stdin, stdout) {
+    this.stdin = stdin;
+    this.stdout = stdout;
+  }
+
   say(message) {
-    console.log(message);
+    this.stdout.write(`${message}\n`);
+  }
+
+  prompt(msg) {
+    tty.setRawMode(true);
+    this.stdout.write(`${msg} [yn]`);
+
+    return new Promise((resolve, reject) => {
+      this.stdin.on('data', (key) => {
+        switch (key) {
+          case 'y':
+            resolve(true);
+
+            break;
+
+          case 'n':
+            resolve(false);
+
+            break;
+
+          default: return;
+        }
+
+        tty.setRawMode(false);
+        this.stdout.write('\n');
+      });
+
+      this.stdin.on('end', () => {
+        tty.setRawMode(false);
+
+        reject(new Error('STDIN closed'));
+      });
+
+    });
   }
 }
 
