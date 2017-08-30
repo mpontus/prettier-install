c161636050129014e371ef71d93a5cd9419c8e20
Michael Pontus <m.pontus@gmail.com>
2017-08-30 21:02:37 +0300

    Refactor addPrettierScript

--- src/modules/addPrettierScript.js
+++ src/modules/addPrettierScript.js
@@ -6,32 +6,19 @@ import { skipWhen, withProgress } from '../helpers/modules';
 
 const DEFAULT_GLOB = '**/*.js';
 
-/*
- * Module decorator
- *
- * This part controls the module invocation in the context of the program.
- */
-
 const hasOption = option => ({ options }) => R.has(option, options);
 
 const eslintInstalled = ({ environment }) => 'eslint' in environment.getProjectDependencies();
 
-// Forego skip checks when user demands script to be installed
 const shouldForce = hasOption('command');
 
 const shouldSkip = R.anyPass([
-  // Skip if prettier script already exists
   ({ environment }) => 'prettier' in environment.getPackageScripts(),
-  // Skip if user does not want script to be added (--no-command)
   hasOption('no-command'),
-  // Skip when eslint can be used to fix files instead
   R.allPass([
-    // Eslint is installed and eslint-plugin will soon be installed
     eslintInstalled,
-    // Unless the user refuses it explicitly
     R.complement(hasOption('no-eslint-plugin')),
   ]),
-  // Skip when user expects eslint plugin to be installed
   hasOption('eslint-plugin'),
 ]);
 
@@ -46,20 +33,16 @@ export const enhance = R.compose(
   withProgress('Adding prettier script'),
 );
 
-/*
- * Module Core
- *
- * This part describes the primary function of the module
- */
+const readFile = filename => promisify(fs.readFile)(filename, 'utf8');
 
-// Read file, modify contents using specified function, then write modified contents back
+const writeFile = R.curry(R.binary(promisify(fs.writeFile)));
+
 const modifyFile = (filename, modifier) => R.composeP(
-  contents => promisify(fs.writeFile)(filename, contents),
+  writeFile(filename),
   modifier,
-  filename => promisify(fs.readFile)(filename, 'utf8'),
+  readFile,
 )(filename);
 
-// Alter JSON string while preserving indentation
 const processJson = modifier => R.compose(
   ([indent, json]) => JSON.stringify(json, null, indent),
   R.juxt([
