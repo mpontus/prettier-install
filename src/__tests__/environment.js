cc90a35adda1664e3bf7ab6e1473897334317d71
Michael Pontus <m.pontus@gmail.com>
2017-09-07 20:15:02 +0300

    Formatting

--- src/__tests__/environment.js
+++ src/__tests__/environment.js
@@ -1,63 +1,68 @@
-import fs from 'fs';
-import childProcess from 'child_process';
-import dedent from 'dedent';
-import * as environment from '../environment';
+import fs from "fs";
+import childProcess from "child_process";
+import dedent from "dedent";
+import * as environment from "../environment";
 
-jest.mock('fs');
-jest.mock('child_process');
+jest.mock("fs");
+jest.mock("child_process");
 
-describe('Environment', () => {
+describe("Environment", () => {
   beforeEach(() => {
     fs._mockReset();
-  })
+  });
 
-  describe('getPacakger', () => {
-    it('returns null when no packager binary can be found', async () => {
-      process.env.PATH = '/usr/bin';
+  describe("getPacakger", () => {
+    it("returns null when no packager binary can be found", async () => {
+      process.env.PATH = "/usr/bin";
 
       const result = await environment.getPackager();
 
       expect(result).toBe(null);
     });
 
-    it('returns yarn when yarn binary exists', async () => {
-      process.env.PATH = '/usr/bin';
-      fs._mockFileAccess('/usr/bin/yarn', fs.constants.X_OK);
+    it("returns yarn when yarn binary exists", async () => {
+      process.env.PATH = "/usr/bin";
+      fs._mockFileAccess("/usr/bin/yarn", fs.constants.X_OK);
 
       const result = await environment.getPackager();
 
-      expect(result).toBe('yarn');
+      expect(result).toBe("yarn");
     });
 
-    it('returns npm when npm binary exists', async () => {
-      process.env.PATH = '/usr/bin';
-      fs._mockFileAccess('/usr/bin/npm', fs.constants.X_OK);
+    it("returns npm when npm binary exists", async () => {
+      process.env.PATH = "/usr/bin";
+      fs._mockFileAccess("/usr/bin/npm", fs.constants.X_OK);
 
       const result = await environment.getPackager();
 
-      expect(result).toBe('npm');
+      expect(result).toBe("npm");
     });
   });
 
-  describe('getDependencies', () => {
-    it('returns empty object when package.json does not exist', async () => {
+  describe("getDependencies", () => {
+    it("returns empty object when package.json does not exist", async () => {
       const result = await environment.getProjectDependencies();
 
       expect(result).toEqual({});
     });
 
-    it('returns empty object when package.json does not contain any dependencies', async () => {
-      fs._mockFileContents('package.json', dedent`{
+    it("returns empty object when package.json does not contain any dependencies", async () => {
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "name": "Foobar"
-      }`);
+      }`
+      );
 
       const result = await environment.getProjectDependencies();
 
       expect(result).toEqual({});
     });
 
-    it('should return all project dependencies', async () => {
-      fs._mockFileContents('package.json', dedent`{
+    it("should return all project dependencies", async () => {
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "dependencies": {
           "lodash": "^3.18.1"
         },
@@ -65,110 +70,117 @@ describe('Environment', () => {
           "babel": "^5.4.13",
           "eslint": "^8.1.0"
         }
-      }`);
+      }`
+      );
 
       const result = await environment.getProjectDependencies();
 
       expect(result).toEqual({
-        lodash: '^3.18.1',
-        babel: '^5.4.13',
-        eslint: '^8.1.0',
+        lodash: "^3.18.1",
+        babel: "^5.4.13",
+        eslint: "^8.1.0"
       });
     });
   });
 
-  describe('getInstalledModules', () => {
-    it('should return all installed modules', async () => {
-      fs._mockDirContents('node_modules', ['lodash', 'prettier']);
+  describe("getInstalledModules", () => {
+    it("should return all installed modules", async () => {
+      fs._mockDirContents("node_modules", ["lodash", "prettier"]);
 
       const result = await environment.getInstalledModules();
 
-      expect(result).toEqual(['lodash', 'prettier']);
+      expect(result).toEqual(["lodash", "prettier"]);
     });
   });
 
-  describe('getPackageScripts', () => {
-    it('returns empty object when package.json does not exist', async () => {
+  describe("getPackageScripts", () => {
+    it("returns empty object when package.json does not exist", async () => {
       const result = await environment.getProjectDependencies();
 
       expect(result).toEqual({});
     });
 
-    it('returns empty object when package.json does not contain any scripts', async () => {
-      fs._mockFileContents('package.json', dedent`{
+    it("returns empty object when package.json does not contain any scripts", async () => {
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "name": "Foobar"
-      }`);
+      }`
+      );
 
       const result = await environment.getProjectDependencies();
 
       expect(result).toEqual({});
     });
 
-    it('should return all package scripts', async () => {
-      fs._mockFileContents('package.json', dedent`{
+    it("should return all package scripts", async () => {
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "scripts": {
           "test": "jest",
           "lint": "eslint"
         }
-      }`);
+      }`
+      );
 
       const result = await environment.getPackageScripts();
 
       expect(result).toEqual({
-        test: 'jest',
-        lint: 'eslint',
+        test: "jest",
+        lint: "eslint"
       });
     });
   });
 
-  describe('pathExists', () => {
-    it('returns false when path is unaccessible', async () => {
-      const result = await environment.pathExists('foo.js');
+  describe("pathExists", () => {
+    it("returns false when path is unaccessible", async () => {
+      const result = await environment.pathExists("foo.js");
 
       expect(result).toBe(false);
     });
 
-    it('returns true when path is accessible', async () => {
-      fs._mockFileAccess('foo.js');
+    it("returns true when path is accessible", async () => {
+      fs._mockFileAccess("foo.js");
 
-      const result = await environment.pathExists('foo.js');
+      const result = await environment.pathExists("foo.js");
 
       expect(result).toBe(true);
     });
   });
 
-  describe('findExecutable', () => {
-    it('returns null when executable can not be found', async () => {
-      process.env.PATH = '/usr/bin:/usr/local/bin';
+  describe("findExecutable", () => {
+    it("returns null when executable can not be found", async () => {
+      process.env.PATH = "/usr/bin:/usr/local/bin";
 
-      const result = await environment.findExecutable('git');
+      const result = await environment.findExecutable("git");
 
       expect(result).toBe(null);
     });
 
-    it('returns path to the found executable', async () => {
-      process.env.PATH = '/usr/bin:/usr/local/bin';
-      fs._mockFileAccess('/usr/local/bin/git', fs.constants.X_OK);
+    it("returns path to the found executable", async () => {
+      process.env.PATH = "/usr/bin:/usr/local/bin";
+      fs._mockFileAccess("/usr/local/bin/git", fs.constants.X_OK);
 
-      const result = await environment.findExecutable('git');
+      const result = await environment.findExecutable("git");
 
-      expect(result).toBe('/usr/local/bin/git');
+      expect(result).toBe("/usr/local/bin/git");
     });
 
-    it('returns false when none of the found files can be executed', async () => {
-      process.env.PATH = '/usr/bin';
-      fs._mockFileAccess('/usr/bin/git', fs.constants.R_OK);
+    it("returns false when none of the found files can be executed", async () => {
+      process.env.PATH = "/usr/bin";
+      fs._mockFileAccess("/usr/bin/git", fs.constants.R_OK);
 
-      const result = await environment.findExecutable('git');
+      const result = await environment.findExecutable("git");
 
       expect(result).toBe(null);
-    })
+    });
   });
 
-  describe('isCleanWorkingTree', () => {
-    it('returns true when working tree is clean', async () => {
+  describe("isCleanWorkingTree", () => {
+    it("returns true when working tree is clean", async () => {
       childProcess.exec.mockImplementation((command, callback) => {
-        expect(command).toBe('git diff-index --quiet HEAD --')
+        expect(command).toBe("git diff-index --quiet HEAD --");
 
         callback(null);
       });
@@ -178,11 +190,11 @@ describe('Environment', () => {
       expect(result).toBe(true);
     });
 
-    it('returns false when working tree is dirty', async () => {
+    it("returns false when working tree is dirty", async () => {
       childProcess.exec.mockImplementation((command, callback) => {
-        expect(command).toBe('git diff-index --quiet HEAD --')
+        expect(command).toBe("git diff-index --quiet HEAD --");
 
-        const error = new Error('Process exitted with status code 1');
+        const error = new Error("Process exitted with status code 1");
         error.code = 1;
 
         callback(error);
@@ -193,11 +205,11 @@ describe('Environment', () => {
       expect(result).toBe(false);
     });
 
-    it('rethrows unrecognized errors', () => {
-      const error = new Error('foo');
+    it("rethrows unrecognized errors", () => {
+      const error = new Error("foo");
 
       childProcess.exec.mockImplementation((command, callback) => {
-        expect(command).toBe('git diff-index --quiet HEAD --')
+        expect(command).toBe("git diff-index --quiet HEAD --");
 
         callback(error);
       });
@@ -208,189 +220,215 @@ describe('Environment', () => {
     });
   });
 
-  describe('findEslintrc', () => {
-    it('returns null if no configuration file exists', async () => {
+  describe("findEslintrc", () => {
+    it("returns null if no configuration file exists", async () => {
       const result = await environment.findEslintrc();
 
       expect(result).toBe(null);
     });
 
-    it('returns null if package.json does not contain eslintConfig section', async () => {
-      fs._mockFileAccess('package.json');
-      fs._mockFileContents('package.json', dedent`{
+    it("returns null if package.json does not contain eslintConfig section", async () => {
+      fs._mockFileAccess("package.json");
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "name": "Foo"
-      }`);
+      }`
+      );
 
       const result = await environment.findEslintrc();
 
       expect(result).toBe(null);
     });
 
-    it('returns package.json when it contains eslintConfig section', async () => {
-      fs._mockFileAccess('package.json');
-      fs._mockFileContents('package.json', dedent`{
+    it("returns package.json when it contains eslintConfig section", async () => {
+      fs._mockFileAccess("package.json");
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "eslintConfig": {}
-      }`);
+      }`
+      );
 
       const result = await environment.findEslintrc();
 
-      expect(result).toBe('package.json');
+      expect(result).toBe("package.json");
     });
 
-    it('returns .eslintrc when no higher priority file exists', async () => {
-      fs._mockFileAccess('.eslintrc');
+    it("returns .eslintrc when no higher priority file exists", async () => {
+      fs._mockFileAccess(".eslintrc");
 
       const result = await environment.findEslintrc();
 
-      expect(result).toBe('.eslintrc');
+      expect(result).toBe(".eslintrc");
     });
 
-    it('returns .eslintrc.json when no higher priority file exists', async () => {
-      fs._mockFileAccess('.eslintrc.json');
+    it("returns .eslintrc.json when no higher priority file exists", async () => {
+      fs._mockFileAccess(".eslintrc.json");
 
       const result = await environment.findEslintrc();
 
-      expect(result).toBe('.eslintrc.json');
+      expect(result).toBe(".eslintrc.json");
     });
 
-    it('returns .eslintrc.yml when no higher priority file exists', async () => {
-      fs._mockFileAccess('.eslintrc.yml');
+    it("returns .eslintrc.yml when no higher priority file exists", async () => {
+      fs._mockFileAccess(".eslintrc.yml");
 
       const result = await environment.findEslintrc();
 
-      expect(result).toBe('.eslintrc.yml');
+      expect(result).toBe(".eslintrc.yml");
     });
 
-    it('returns .eslintrc.yaml when no higher priority file exists', async () => {
-      fs._mockFileAccess('.eslintrc.yaml');
+    it("returns .eslintrc.yaml when no higher priority file exists", async () => {
+      fs._mockFileAccess(".eslintrc.yaml");
 
       const result = await environment.findEslintrc();
 
-      expect(result).toBe('.eslintrc.yaml');
+      expect(result).toBe(".eslintrc.yaml");
     });
 
-    it('returns .eslintrc.js when it exists', async () => {
-      fs._mockFileAccess('.eslintrc.js');
+    it("returns .eslintrc.js when it exists", async () => {
+      fs._mockFileAccess(".eslintrc.js");
 
       const result = await environment.findEslintrc();
 
-      expect(result).toBe('.eslintrc.js');
+      expect(result).toBe(".eslintrc.js");
     });
   });
 
-  describe('eslintPresets', () => {
-    it('should throw error when no supported eslint config is found', () => {
+  describe("eslintPresets", () => {
+    it("should throw error when no supported eslint config is found", () => {
       const result = environment.eslintPresets();
 
       return expect(result).rejects.toEqual(expect.any(Error));
     });
 
-    it('should return single preset from .eslintrc.json', async () => {
-      fs._mockFileAccess('.eslintrc.json');
-      fs._mockFileContents('.eslintrc.json', dedent`{
+    it("should return single preset from .eslintrc.json", async () => {
+      fs._mockFileAccess(".eslintrc.json");
+      fs._mockFileContents(
+        ".eslintrc.json",
+        dedent`{
         "extends": "foo"
-      }`);
+      }`
+      );
 
       const result = await environment.eslintPresets();
 
-      return expect(result).toEqual(['foo']);
+      return expect(result).toEqual(["foo"]);
     });
 
-    it('should return presets from .eslintrc.json', async () => {
-      fs._mockFileAccess('.eslintrc.json');
-      fs._mockFileContents('.eslintrc.json', dedent`{
+    it("should return presets from .eslintrc.json", async () => {
+      fs._mockFileAccess(".eslintrc.json");
+      fs._mockFileContents(
+        ".eslintrc.json",
+        dedent`{
         "extends": ["foo", "bar"]
-      }`)
+      }`
+      );
 
       const result = await environment.eslintPresets();
 
-      expect(result).toEqual(['foo', 'bar']);
+      expect(result).toEqual(["foo", "bar"]);
     });
 
-    it('should return presets from package.json', async () => {
-      fs._mockFileAccess('package.json');
-      fs._mockFileContents('package.json', dedent`{
+    it("should return presets from package.json", async () => {
+      fs._mockFileAccess("package.json");
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "eslintConfig": {
           "extends": ["foo", "bar"]
         }
-      }`)
+      }`
+      );
 
       const result = await environment.eslintPresets();
 
-      expect(result).toEqual(['foo', 'bar']);
+      expect(result).toEqual(["foo", "bar"]);
     });
   });
 
-  describe('eslintPlugins', () => {
-    it('should throw error when no supported eslint config is found', async () => {
+  describe("eslintPlugins", () => {
+    it("should throw error when no supported eslint config is found", async () => {
       const result = environment.eslintPlugins();
 
       return expect(result).rejects.toEqual(expect.any(Error));
     });
 
-    it('should return plugins from .eslintrc.json', async () => {
-      fs._mockFileAccess('.eslintrc.json');
-      fs._mockFileContents('.eslintrc.json', dedent`{
+    it("should return plugins from .eslintrc.json", async () => {
+      fs._mockFileAccess(".eslintrc.json");
+      fs._mockFileContents(
+        ".eslintrc.json",
+        dedent`{
         "plugins": ["foo", "bar"]
-      }`)
+      }`
+      );
 
       const result = await environment.eslintPlugins();
 
-      expect(result).toEqual(['foo', 'bar']);
+      expect(result).toEqual(["foo", "bar"]);
     });
 
-    it('should return presets from package.json', async () => {
-      fs._mockFileAccess('package.json');
-      fs._mockFileContents('package.json', dedent`{
+    it("should return presets from package.json", async () => {
+      fs._mockFileAccess("package.json");
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "eslintConfig": {
           "plugins": ["foo", "bar"]
         }
-      }`)
+      }`
+      );
 
       const result = await environment.eslintPlugins();
 
-      expect(result).toEqual(['foo', 'bar']);
+      expect(result).toEqual(["foo", "bar"]);
     });
   });
 
-  describe('eslintRules', () => {
-    it('should throw error when no supported eslint config is found', async () => {
+  describe("eslintRules", () => {
+    it("should throw error when no supported eslint config is found", async () => {
       const result = environment.eslintRules();
 
       return expect(result).rejects.toEqual(expect.any(Error));
     });
 
-    it('should return rules from .eslintrc.json', async () => {
-      fs._mockFileAccess('.eslintrc.json');
-      fs._mockFileContents('.eslintrc.json', dedent`{
+    it("should return rules from .eslintrc.json", async () => {
+      fs._mockFileAccess(".eslintrc.json");
+      fs._mockFileContents(
+        ".eslintrc.json",
+        dedent`{
         "rules": {
           "semi": ["warn", "always"]
         }
-      }`)
+      }`
+      );
 
       const result = await environment.eslintRules();
 
       expect(result).toEqual({
-        semi: ['warn', 'always'],
+        semi: ["warn", "always"]
       });
     });
 
-    it('should return presets from package.json', async () => {
-      fs._mockFileAccess('package.json');
-      fs._mockFileContents('package.json', dedent`{
+    it("should return presets from package.json", async () => {
+      fs._mockFileAccess("package.json");
+      fs._mockFileContents(
+        "package.json",
+        dedent`{
         "eslintConfig": {
           "rules": {
             "semi": ["warn", "always"]
           }
         }
-      }`)
+      }`
+      );
 
       const result = await environment.eslintRules();
 
       expect(result).toEqual({
-        semi: ['warn', 'always'],
+        semi: ["warn", "always"]
       });
     });
   });
-
 });
--- src/environment.js
+++ src/environment.js
@@ -1,15 +1,18 @@
 // TODO: try fs-extra
-import R from 'ramda';
-import { promisify } from 'util';
-import path from 'path';
-import fs from 'fs';
-import childProcess from 'child_process';
+import R from "ramda";
+import { promisify } from "util";
+import path from "path";
+import fs from "fs";
+import childProcess from "child_process";
 
 export async function getProjectDependencies() {
   let projectMetadata;
 
   try {
-    const projectMetadataContents = await promisify(fs.readFile)('package.json', 'utf8');
+    const projectMetadataContents = await promisify(fs.readFile)(
+      "package.json",
+      "utf8"
+    );
 
     projectMetadata = JSON.parse(projectMetadataContents);
   } catch (error) {
@@ -22,99 +25,101 @@ export async function getProjectDependencies() {
 }
 
 export function getInstalledModules() {
-  return promisify(fs.readDir)('node_modules');
+  return promisify(fs.readDir)("node_modules");
 }
 
 export async function getPackageScripts() {
   let projectMetadata;
 
   try {
-    const projectMetadataContents = await promisify(fs.readFile)('package.json', 'utf8');
+    const projectMetadataContents = await promisify(fs.readFile)(
+      "package.json",
+      "utf8"
+    );
 
     projectMetadata = JSON.parse(projectMetadataContents);
   } catch (error) {
     return {};
   }
 
-
   return projectMetadata.scripts || {};
 }
 
 export function pathExists(path) {
-  return promisify(fs.access)(path)
-    .then(() => true, () => false);
+  return promisify(fs.access)(path).then(() => true, () => false);
 }
 
 export function findExecutable(name) {
-  const isExecutable = path =>
-    promisify(fs.access)(path, fs.constants.X_OK);
+  const isExecutable = path => promisify(fs.access)(path, fs.constants.X_OK);
 
-  return process.env.PATH.split(':').map(
-    dir => path.resolve(dir, name),
-  ).reduceRight(
-    (next, path) => () => isExecutable(path)
-      .then(() => path, () => next()),
-    () => Promise.resolve(null),
-  )();
+  return process.env.PATH
+    .split(":")
+    .map(dir => path.resolve(dir, name))
+    .reduceRight(
+      (next, path) => () => isExecutable(path).then(() => path, () => next()),
+      () => Promise.resolve(null)
+    )();
 }
 
 export async function getPackager() {
-  if (await findExecutable('yarn')) {
-    return 'yarn';
+  if (await findExecutable("yarn")) {
+    return "yarn";
   }
 
-  if (await findExecutable('npm')) {
-    return 'npm';
+  if (await findExecutable("npm")) {
+    return "npm";
   }
 
   return null;
 }
 
 export function isCleanWorkingTree() {
-  return promisify(childProcess.exec)('git diff-index --quiet HEAD --')
-    .then(() => true, (error) => {
+  return promisify(childProcess.exec)("git diff-index --quiet HEAD --").then(
+    () => true,
+    error => {
       if (error.code === 1) {
         return false;
       }
 
       return Promise.reject(error);
-    });
+    }
+  );
 }
 
 export function findEslintrc() {
   const fileExists = filename =>
-    promisify(fs.access)(filename)
-      .then(() => true, () => false)
+    promisify(fs.access)(filename).then(() => true, () => false);
 
   const jsonFileHasSection = section => filename =>
-    promisify(fs.readFile)(filename)
-      .then(contents => section in JSON.parse(contents));
+    promisify(fs.readFile)(filename).then(
+      contents => section in JSON.parse(contents)
+    );
 
   const files = [
-    '.eslintrc.js',
-    '.eslintrc.yaml',
-    '.eslintrc.yml',
-    '.eslintrc.json',
-    '.eslintrc',
-    'package.json',
+    ".eslintrc.js",
+    ".eslintrc.yaml",
+    ".eslintrc.yml",
+    ".eslintrc.json",
+    ".eslintrc",
+    "package.json"
   ];
 
   const tests = {
-    '.eslintrc.js': [fileExists],
-    '.eslintrc.yaml': [fileExists],
-    '.eslintrc.yml': [fileExists],
-    '.eslintrc.json': [fileExists],
-    '.eslintrc': [fileExists],
-    'package.json': [fileExists, jsonFileHasSection('eslintConfig')],
+    ".eslintrc.js": [fileExists],
+    ".eslintrc.yaml": [fileExists],
+    ".eslintrc.yml": [fileExists],
+    ".eslintrc.json": [fileExists],
+    ".eslintrc": [fileExists],
+    "package.json": [fileExists, jsonFileHasSection("eslintConfig")]
   };
 
   return files.reduceRight(
-    (next, path) => () => tests[path].reduceRight(
-        (next, pred) => () => pred(path)
-          .then(result => result && next()),
-        () => Promise.resolve(true),
-      )().then(result => result ? path : next()),
-    () => Promise.resolve(null),
+    (next, path) => () =>
+      tests[path].reduceRight(
+        (next, pred) => () => pred(path).then(result => result && next()),
+        () => Promise.resolve(true)
+      )().then(result => (result ? path : next())),
+    () => Promise.resolve(null)
   )();
 }
 
@@ -122,17 +127,20 @@ export async function getEslintConfig() {
   const filename = await this.findEslintrc();
 
   switch (filename) {
-    case '.eslintrc.json':
-      return promisify(fs.readFile)(filename, 'utf8')
-        .then(contents => JSON.parse(contents));
-    case 'package.json':
-      return promisify(fs.readFile)(filename, 'utf8')
+    case ".eslintrc.json":
+      return promisify(fs.readFile)(filename, "utf8").then(contents =>
+        JSON.parse(contents)
+      );
+    case "package.json":
+      return promisify(fs.readFile)(filename, "utf8")
         .then(contents => JSON.parse(contents))
         .then(metadata => metadata.eslintConfig || {});
     case null:
-      throw new Error('Eslint configuration file is not found')
+      throw new Error("Eslint configuration file is not found");
     default:
-      throw new Error(`Eslint configuration in file ${filename} is not supported`)
+      throw new Error(
+        `Eslint configuration in file ${filename} is not supported`
+      );
   }
 }
 
@@ -143,11 +151,9 @@ export function eslintPresets() {
 }
 
 export function eslintPlugins() {
-  return this.getEslintConfig()
-    .then(config => config.plugins || []);
+  return this.getEslintConfig().then(config => config.plugins || []);
 }
 
 export function eslintRules() {
-  return this.getEslintConfig()
-    .then(config => config.rules || {});
+  return this.getEslintConfig().then(config => config.rules || {});
 }
